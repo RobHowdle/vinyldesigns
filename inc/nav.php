9f180d8f82bae48b8b3eed4d07862ebc3ff6b97f
Rob Howdle <robhowdle94@gmail.com>
2026-01-08 21:31:27 +0000

    Refactor navigation to dynamically highlight active page and improve accessibility

--- inc/nav.php
+++ inc/nav.php
@@ -1,3 +1,14 @@
+<?php
+    $currentPage = basename($_SERVER['SCRIPT_NAME'] ?? '');
+    if ($currentPage === '') {
+        $currentPage = 'index.php';
+    }
+
+    $isActive = function (string $page) use ($currentPage): bool {
+        return $page === $currentPage;
+    };
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark" data-aos="fade-up">
     <div class="nav-button text-center">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,20 +20,20 @@
 
         <ul class="navbar-nav nav-fill w-100">
 
-        <li class="nav-item active">
-            <a class="nav-link" href="index.php" data-aos="fade-right" data-aos-delay="300">Home <span class="sr-only">(current)</span></a>
+        <li class="nav-item<?php echo $isActive('index.php') ? ' active' : ''; ?>">
+            <a class="nav-link" href="index.php" <?php echo $isActive('index.php') ? 'aria-current="page"' : ''; ?> data-aos="fade-right" data-aos-delay="300">Home<?php echo $isActive('index.php') ? ' <span class="sr-only">(current)</span>' : ''; ?></a>
         </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="services.php" data-aos="fade-right" data-aos-delay="200">Services</a>
+        <li class="nav-item<?php echo $isActive('services.php') ? ' active' : ''; ?>">
+            <a class="nav-link" href="services.php" <?php echo $isActive('services.php') ? 'aria-current="page"' : ''; ?> data-aos="fade-right" data-aos-delay="200">Services<?php echo $isActive('services.php') ? ' <span class="sr-only">(current)</span>' : ''; ?></a>
         </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="testimonials.php" data-aos="fade-left" data-aos-delay="200">Testimonials</a>
+        <li class="nav-item<?php echo $isActive('testimonials.php') ? ' active' : ''; ?>">
+            <a class="nav-link" href="testimonials.php" <?php echo $isActive('testimonials.php') ? 'aria-current="page"' : ''; ?> data-aos="fade-left" data-aos-delay="200">Testimonials<?php echo $isActive('testimonials.php') ? ' <span class="sr-only">(current)</span>' : ''; ?></a>
         </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="contact.php"data-aos="fade-left" data-aos-delay="300">Contact</a>
+        <li class="nav-item<?php echo $isActive('contact.php') ? ' active' : ''; ?>">
+            <a class="nav-link" href="contact.php" <?php echo $isActive('contact.php') ? 'aria-current="page"' : ''; ?> data-aos="fade-left" data-aos-delay="300">Contact<?php echo $isActive('contact.php') ? ' <span class="sr-only">(current)</span>' : ''; ?></a>
         </li>
 
         </ul>
